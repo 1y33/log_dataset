e9ad9d55d37cc6c4c9f2ddfcdcc6a3b8e71138a6
cata <cata@>
2024-10-18 00:02:40 +0300

    created the inference function created the best inference params search . Need to update it better

--- inference.py
+++ inference.py
@@ -1,10 +1,18 @@
+from pycparser.ply.yacc import restart
+
 import get_model
 import supervision as sv
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-def inference_slicing(image_path,model_path,imgsz,overlap_wh,iou_trashold,overlap_filter="NMS",show=True):
+model_path = "runs/detect/yoloV5-refine--EP:20-BS:16+1e-05-cos_lr-False-drp-0.4+AdamW/weights/best.pt"
+image_path = "images/test_image.jpeg"
+#
+# inference_slicing(image_path,model_path,imgsz=256,iou_trashold=0.5,overlap_wh=(0.3,0.3),overlap_filter="NMS")
+
+
+def inference_slicing(image_path,model_path,imgsz,overlap_wh,iou_trashold,confidence=0.3,overlap_filter="NMS",show=True,return_count=False):
     if overlap_filter == "NMS":
         overlap_filter = sv.OverlapFilter.NON_MAX_SUPPRESSION
     else:
@@ -31,7 +39,7 @@ def inference_slicing(image_path,model_path,imgsz,overlap_wh,iou_trashold,overla
                                 )
 
     detections = slicer(image)
-    detections = detections[detections.confidence > 0.3]
+    detections = detections[detections.confidence > confidence]
 
     bbox_annotator = sv.BoxAnnotator()
     annotated_image = bbox_annotator.annotate(scene=image.copy(), detections=detections)
@@ -44,11 +52,62 @@ def inference_slicing(image_path,model_path,imgsz,overlap_wh,iou_trashold,overla
 
     print(f"Number of detections: {len(detections.confidence)}")
 
+    if return_count:
+        return int(len(detections.confidence))
 
-# model_path = "runs/detect/yoloV5-refine--EP:20-BS:16+1e-05-cos_lr-False-drp-0.4+AdamW/weights/best.pt"
-# image_path = "images/test_image.jpeg"
-#
-# inference_slicing(image_path,model_path,imgsz=256,iou_trashold=0.5,overlap_wh=(0.3,0.3),overlap_filter="NMS")
+def best_inference_params(model_path,image_path,target_count):
+    best_count = 0
+
+    imgsz_list = [256,320]
+    iou_trashold_list = [0.45,0.5]
+    confidence_list = [0.5,0.6]
+    overlap_wh_list = [0.4,0.5]
+    overlap_filter_list =["NMS","NMG"]
+
+    values = []
+    for imgsz in imgsz_list:
+        for iou_trashold in iou_trashold_list:
+            for confidence in confidence_list:
+                for overlap_filter in overlap_filter_list:
+                    for overlap_wh in overlap_wh_list:
+                        value = inference_slicing(
+                            model_path=model_path,
+                            image_path=image_path,
+                            imgsz=imgsz,
+                            overlap_wh=(overlap_wh,overlap_wh),
+                            iou_trashold=iou_trashold,
+                            confidence=confidence,
+                            overlap_filter=overlap_filter,
+                            show=False,
+                            return_count=True
+                        )
+                        value_dict = {
+                            "value": value,
+                            "imgsz": imgsz,
+                            "iou_trashold": iou_trashold,
+                            "confidence": confidence,
+                            "overlap_filter": overlap_filter,
+                            "overlap_wh": (overlap_wh,overlap_wh),
+                        }
+                        values.append(value_dict)
+
+    closest_value = min(values, key=lambda x: abs(x["value"] - target_count))["value"]
+    result = [value_dict for value_dict in values if value_dict["value"] == closest_value]
+
+    print("Value: " ,result["value"])
+    print("Imgsz: " ,result["imgsz"])
+    print("Iou Trashold: ",result["iou_trashold"])
+    print("Confidence: ",result["confidence"])
+    print("Overlap Filter: ",result["overlap_filter"])
+    print("Overlap_Wh: ",result["overlap_wh"])
+
+    return result
+
+
+model_path = "runs/detect/yoloV5-refine--EP:20-BS:16+1e-05-cos_lr-False-drp-0.4+AdamW/weights/best.pt"
+image_path = "images/test_image.jpeg"
+
+result = best_inference_params(model_path,image_path,104)
 
 
 
