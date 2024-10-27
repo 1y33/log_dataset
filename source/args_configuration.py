34be04f2441e0e1f0d55c25131c7a0181f08cbef
1y33 <catalin.milu.2004@gmail.com>
2024-10-27 12:27:14 +0200

    Added configs for VOC and COCO

--- source/args_configuration.py
+++ source/args_configuration.py
@@ -22,3 +22,45 @@ class LogDatasetConfig:
     project_name = "1y33/test"
     experiment_name = run_name
     tags = ['Ultralytics', 'Training']
+
+class COCOConfing:
+    PATH_TO_DATA = "coco.yaml"
+    PATH_TO_MODEL = None
+    run_name = "training_coco"
+    name = f"{run_name}_"
+    epochs = 300
+    batch_size = 128
+    dropout = 0.2
+    lr0 = 1e-3
+    lrf = 1e-5
+    imgsz = 1024
+    cos_lr = True
+    optimizer = "AdamW"
+    save_period = 10
+    cache = True
+    seed = 36
+
+    project_name = "1y33/test"
+    experiment_name = run_name
+    tags = [ 'Training','CoCo']
+
+class VOCConfing:
+    PATH_TO_DATA = "VOC.yaml"
+    PATH_TO_MODEL = None
+    run_name = "training_voc"
+    name = f"{run_name}_"
+    epochs = 300
+    batch_size = 128
+    dropout = 0.1
+    lr0 = 1e-3
+    lrf = 1e-4
+    imgsz = 1024
+    cos_lr = True
+    optimizer = "AdamW"
+    save_period = 10
+    cache = True
+    seed = 36
+
+    project_name = "1y33/test"
+    experiment_name = run_name
+    tags = [ 'Training','VOC']
