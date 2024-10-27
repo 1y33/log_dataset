798e610f4edb0c1f46dc7cafb87be69ec6e842ba
1y33 <catalin.milu.2004@gmail.com>
2024-10-27 15:16:26 +0200

    install libraries

--- source/train_model_file.py
+++ source/train_model_file.py
@@ -9,13 +9,13 @@ def train():
     m = get_model.Model(main_model)
 
     # Training for COCO:
-    m.get_dataset(COCO_args.PATH_TO_DATA)
     m.add_callbacks(COCO_args.project_name, COCO_args.experiment_name, COCO_args.tags)
+    m.get_dataset(COCO_args.PATH_TO_DATA)
     m.train_model(COCO_args)
 
     # Training for VOC
-    m.get_dataset(VOC_args.PATH_TO_DATA)
     m.add_callbacks(VOC_args.project_name, VOC_args.experiment_name, VOC_args.tags)
+    m.get_dataset(VOC_args.PATH_TO_DATA)
     m.train_model(VOC_args)
 
 
