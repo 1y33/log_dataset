9a292083c7d11a6853ddbd704ab85e1de08b4243
1y33 <catalin.milu.2004@gmail.com>
2024-10-30 13:31:35 +0200

    -

--- source/train_model_file.py
+++ source/train_model_file.py
@@ -13,6 +13,7 @@ def train():
     # m.get_dataset(COCO_args.PATH_TO_DATA)
     # m.train_model(COCO_args)
 
+
     # Training for VisDrone
     # m.add_callbacks(VOC_args.project_name, VOC_args.experiment_name, VOC_args.tags)
     m.get_dataset(VisDrone.PATH_TO_DATA)
